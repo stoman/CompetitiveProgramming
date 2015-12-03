85f82ee3a9ebdc5afab6c5328ce50b7f9ef4110d
Stefan Toman <git@stoman.de>
2015-12-03 00:02:25 +0000

    add problem template

---
+++ problems/template/submissions/accepted/stefan.cpp
@@ -0,0 +1,17 @@
+//Author: Stefan Toman
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	for (int i = 0; i < t; ++i) {
+        int n;
+		scanf("%d", &n);
+
+		printf("%d\n", n);
+	}
+	return 0;
+}
