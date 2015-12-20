1a86063afa1ba8ee16c40a49a493fdd673285ef8
Stefan Toman <git@stoman.de>
2015-12-20 22:04:31 +0000

    add problem chefandsubarrays

---
+++ problems/chefandsubarrays/submissions/accepted/stefan.cpp
@@ -0,0 +1,34 @@
+//Author: Stefan Toman
+
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	for (int i = 0; i < t; ++i) {
+        int n;
+		scanf("%d", &n);
+		long long a[n];
+		for (int j = 0; j < n; j++) {
+			scanf("%lld", &a[j]);
+		}
+		int r = 0;
+		for (int x = 0; x < n; x++) {
+			long long sum = 0;
+			long long product = 1;
+			for (int y = x; y < n; y++) {
+				sum += a[y];
+				product *= a[y];
+				if(sum == product) {
+					r++;
+				}
+			}
+		}
+		printf("%d\n", r);
+	}
+	return 0;
+}
