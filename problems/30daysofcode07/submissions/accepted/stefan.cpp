72b4e1bab9ddd17ab9e325a3f7e485d9c41ce82d
Stefan Toman <git@stoman.de>
2016-01-09 22:44:24 +0000

    add problem 30daysofcode07

---
+++ problems/30daysofcode07/submissions/accepted/stefan.cpp
@@ -0,0 +1,16 @@
+//Author: Stefan Toman
+
+#include <stdio.h>
+
+int main() {
+    int n;
+	scanf("%d", &n);
+	int a[n];
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = n-1; i >= 0; i--) {
+		printf("%d%s", a[i], i == 0 ? "\n" : " ");
+	}
+	return 0;
+}
