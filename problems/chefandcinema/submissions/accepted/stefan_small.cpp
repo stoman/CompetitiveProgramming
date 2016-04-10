d901ed0dca8c800572a4d1524ebf38192f12beef
Stefan Toman <git@stoman.de>
2016-04-10 15:27:13 +0200

    fix problem chefandcinema

--- problems/chefandcinema/submissions/accepted/stefan_small.cpp
+++ problems/chefandcinema/submissions/accepted/stefan_small.cpp
@@ -1,6 +1,6 @@
 //Author: Stefan Toman
 
-#include <stdio.h>
+#include <iostream>
 #include <string.h>
 #include <algorithm>
 
@@ -8,10 +8,10 @@ using namespace std;
 
 int main() {
 	int t;
-	scanf("%d", &t);
+	cin >> t;
 	for (int i = 0; i < t; ++i) {
         long long n, m, z, l, r, b;
-		scanf("%I64d %I64d %I64d %I64d %I64d %I64d", &n, &m, &z, &l, &r, &b);
+		cin >> n >> m >> z >> l >> r >> b;
 		long long seated = 0, a = 0;
 
 		//even number of seats
@@ -36,35 +36,29 @@ int main() {
 	        long long ls = min(l, m-1);
 	        l -= ls;
 	        taken += ls;
-    		//printf("-> ls %I64d\n", ls);
 	        
 	        //Rs from the right, leave one free
             long long rs = min(m-taken-1, r);
             r -= rs;
             taken += rs;
-    		//printf("-> rs %I64d\n", rs);
             
             //single empty seat?
             if(taken == m-1) {
                 if(b > 0) {
                     b--;
                     taken++;
-            		//printf("-> +b\n");
                 }
                 else if(l > 0) {
                     l--;
                     taken++;
-            		//printf("-> +l\n");
                 }
                 else if(r > 0) {
                     r--;
                     taken++;
-            		//printf("-> +r\n");
                 }
                 else if(z > 0) {
                     z--;
                     taken++;
-            		//printf("-> +z\n");
                 }
             }
             else {
@@ -73,7 +67,6 @@ int main() {
                 b -= k;
                 z -= k;
                 taken += 2*k;
-        		//printf("-> k %I64d\n", k);
                 
                 //only B or Z remaining
                 if(b > 0) {
@@ -81,24 +74,21 @@ int main() {
                     long long bs = min(b, (m-taken+1)/2);
                     b -= bs;
                     taken += bs;
-            		//printf("-> bs %I64d\n", bs);
                 }
                 else {
                     //only Z
                     long long zs = max((long long) 0, min(z, m-taken));
                     z -= zs;
                     taken += zs;
-            		//printf("-> zs %I64d\n", zs);
                 }
             }
             
             //sum up
             seated += taken;
             n--;
-    		//printf("=> taken %I64d\n", taken);
 		}
       
-		printf("%I64d\n", seated);
+            cout << seated << endl;
 	}
 	return 0;
 }
