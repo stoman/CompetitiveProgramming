f29d31b8383222ade16806d4ff7d9a70823ce0a6
Stefan Toman <git@stoman.de>
2016-04-10 17:05:31 +0200

    fix problem thepriceiscorrect

--- problems/thepriceiscorrect/submissions/accepted/stefan.cpp
+++ problems/thepriceiscorrect/submissions/accepted/stefan.cpp
@@ -1,6 +1,6 @@
 //Author: Stefan Toman
 
-#include <stdio.h>
+#include <iostream>
 #include <string.h>
 #include <algorithm>
 #include <map>
@@ -10,13 +10,13 @@ using namespace std;
 
 int main() {
 	int t;
-	scanf("%d", &t);
+	cin >> t;
 	for (int i = 0; i < t; ++i) {
         long n, p;
-		scanf("%ld %ld", &n, &p);
+		cin >> n >> p;
 		long b[n];
 		for (long j = 0; j < n; j++) {
-			scanf("%ld", &b[j]);
+			cin >> b[j];
 		}
 		long long r = 0;
 		long right = -1, sum = 0;
@@ -28,7 +28,7 @@ int main() {
 			r += right - left + 1;
 			sum -= b[left];
 		}
-		printf("Case #%d: %I64d\n", i+1, r);
+		cout << "Case #" << i+1 << ": " << r << endl;
 	}
 	return 0;
 }
