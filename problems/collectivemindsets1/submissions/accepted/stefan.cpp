8fa7bc949ee7b100201e779a60f8512b2f7d1152
Stefan Toman <git@stoman.de>
2016-07-10 15:35:01 +0200

    add problem collectivemindsets1

---
+++ problems/collectivemindsets1/submissions/accepted/stefan.cpp
@@ -0,0 +1,15 @@
+//Author: Stefan Toman
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+	cout << 1+(n-1)/2 << endl;
+	return 0;
+}
+
