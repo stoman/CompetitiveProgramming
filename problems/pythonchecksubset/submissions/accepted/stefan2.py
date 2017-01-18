491d73c46b8ade82bde9f09b3f61ec1e9ed8c4d9
Stefan Toman <git@stoman.de>
2017-01-18 14:23:18 +0100

    fix build by adding a shebang and a new package

--- problems/pythonchecksubset/submissions/accepted/stefan2.py
+++ problems/pythonchecksubset/submissions/accepted/stefan2.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+#Author: Stefan Toman
+
 for i in range(int(input())): #More than 4 lines will result in 0 score. Blank lines won't be counted. 
     a = int(input()); A = set(input().split()) 
     b = int(input()); B = set(input().split())
