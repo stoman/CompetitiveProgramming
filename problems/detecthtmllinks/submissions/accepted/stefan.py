57c1c975a83737e45061ed7853bdf82cba560c85
Stefan Toman <git@stoman.de>
2017-03-09 17:01:59 +0100

    add problem detecthtmllinks

---
+++ problems/detecthtmllinks/submissions/accepted/stefan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+import re
+
+#Author: Stefan Toman
+
+if __name__ == '__main__':
+    #read input
+    n = int(raw_input())
+    s = ""
+    for _ in range(n):
+        s += raw_input()
+
+    #print output
+    for r in re.finditer("<a ([^<>]*)href=['\"](\S*)['\"]([^<>]*)>(.*?)</a>", s):
+        print "%s,%s" % (r.groups()[1].strip(), re.sub("<(.*?)>", "", r.groups()[3]).strip())
