54f13225c4fa9334675ed75866d5428797d2f24c
Stefan Toman <git@stoman.de>
2020-12-05 12:58:15 +0100

    Add problems 2020adventofcode05a and 2020adventofcode05b

---
+++ problems/2020adventofcode05a/submissions/accepted/Stefan.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun seatId(partitioning: String): Int {
+  var r = 0
+  var back = 64 * 8
+  var right = 4
+  for (c in partitioning) {
+    when (c) {
+      'F' -> back /= 2
+      'B' -> {
+        r += back
+        back /= 2
+      }
+      'L' -> right /= 2
+      'R' -> {
+        r += right
+        right /= 2
+      }
+    }
+  }
+  return r
+}
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+
+  var r = 0
+  while (s.hasNext()) {
+    r = r.coerceAtLeast(seatId(s.next()))
+  }
+  println(r)
+}
---
+++ problems/2020adventofcode05b/submissions/accepted/Stefan.kt
@@ -0,0 +1,37 @@
+import java.util.*
+
+fun seatId(partitioning: String): Int {
+  var r = 0
+  var back = 64 * 8
+  var right = 4
+  for (c in partitioning) {
+    when (c) {
+      'F' -> back /= 2
+      'B' -> {
+        r += back
+        back /= 2
+      }
+      'L' -> right /= 2
+      'R' -> {
+        r += right
+        right /= 2
+      }
+    }
+  }
+  return r
+}
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+
+  val seats = mutableSetOf<Int>()
+  while (s.hasNext()) {
+    seats.add(seatId(s.next()))
+  }
+
+  for(seat in seats) {
+    if(seat +1 !in seats && seat + 2 in seats) {
+      println(seat + 1)
+    }
+  }
+}
