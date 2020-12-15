5ae0fa04f9f91e280a440aea45f94bbc055888e4
Stefan Toman <git@stoman.de>
2020-12-15 22:52:36 +0100

    Add problems 2020adventofcode15a and 2020adventofcode15b

---
+++ problems/2020adventofcode15a/submissions/accepted/Stefan.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`).useDelimiter(",|\\s")
+  var turn = 0
+  var last = -1
+  val lastMentioned = mutableMapOf<Int, Int>()
+  while (s.hasNext()) {
+    lastMentioned[last] = turn
+    last = s.nextInt()
+    turn++
+  }
+  while(turn < 2020) {
+    val next = if(lastMentioned.containsKey(last)) turn - lastMentioned[last]!! else 0
+    lastMentioned[last] = turn
+    last = next
+    turn++
+  }
+  println(last)
+}
