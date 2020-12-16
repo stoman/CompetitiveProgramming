a585b589fb04d3e51f8277975f0d15b0790fb5a4
Stefan Toman <git@stoman.de>
2020-12-16 22:48:24 +0100

    Add problems 2020adventofcode16a and 2020adventofcode16b

---
+++ problems/2020adventofcode16a/submissions/accepted/Stefan.kt
@@ -0,0 +1,23 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`).useDelimiter("""\s\syour ticket:\s|\s\snearby tickets:\s""")
+
+  // Read ranges.
+  val sRanges = Scanner(s.next()).useDelimiter(""":\s|\sor\s|-|\n""")
+  val ranges = mutableMapOf<String, Set<IntRange>>()
+  while (sRanges.hasNext()) {
+    val name = sRanges.next()
+    ranges[name] = setOf(sRanges.nextInt()..sRanges.nextInt(), sRanges.nextInt()..sRanges.nextInt())
+  }
+
+  // Read tickets
+  var myTicket: List<Int> = s.next().split(",").map { it.toInt() }
+  val sTickets = Scanner(s.next())
+  val tickets = mutableListOf<List<Int>>()
+  while (sTickets.hasNext()) {
+    tickets.add(sTickets.next().split(",").map { it.toInt() })
+  }
+
+  println(tickets.flatten().filter { i -> ranges.values.flatten().none { i in it } }.sum())
+}
---
+++ problems/2020adventofcode16b/submissions/accepted/Stefan.kt
@@ -0,0 +1,41 @@
+import java.math.BigInteger
+import java.util.*
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`).useDelimiter("""\s\syour ticket:\s|\s\snearby tickets:\s""")
+
+  // Read ranges.
+  val sRanges = Scanner(s.next()).useDelimiter(""":\s|\sor\s|-|\n""")
+  val ranges = mutableMapOf<String, Set<IntRange>>()
+  while (sRanges.hasNext()) {
+    val name = sRanges.next()
+    ranges[name] = setOf(sRanges.nextInt()..sRanges.nextInt(), sRanges.nextInt()..sRanges.nextInt())
+  }
+
+  // Read tickets
+  var myTicket: List<Int> = s.next().split(",").map { it.toInt() }
+  val sTickets = Scanner(s.next())
+  val tickets = mutableListOf<List<Int>>()
+  while (sTickets.hasNext()) {
+    tickets.add(sTickets.next().split(",").map { it.toInt() })
+  }
+
+  // Determine meaning of fields.
+  val validTickets = tickets.filter { ticket -> ticket.none { i -> ranges.values.flatten().none { i in it } } }
+  var possible = mutableMapOf<Int, Set<String>>()
+  for (i in myTicket.indices) {
+    possible[i] =
+      ranges.filter { entry -> validTickets.map { it[i] }.all { nr -> entry.value.any { nr in it } } }.map { it.key }
+        .toSet()
+  }
+  val mapping = mutableMapOf<String, Int>()
+  while (true) {
+    val next: Int = possible.keys.firstOrNull { possible[it]!!.size == 1 } ?: break
+    val name: String = possible[next]!!.first()
+    mapping[name] = next
+    possible = possible.mapValues { it.value.filter { n -> n != name }.toSet() }.toMutableMap()
+  }
+
+  println(mapping.filterKeys { it.startsWith("departure") }.map { myTicket[it.value].toBigInteger() }
+    .reduce(BigInteger::times))
+}
