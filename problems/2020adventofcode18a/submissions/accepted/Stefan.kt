1fd4f3745c411a1914587010b802cdcd08e70d6c
Stefan Toman <git@stoman.de>
2020-12-18 12:22:48 +0100

    Add problems 2020adventofcode18a and 2020adventofcode18b

---
+++ problems/2020adventofcode18a/submissions/accepted/Stefan.kt
@@ -0,0 +1,33 @@
+import java.math.BigInteger
+import java.util.*
+
+fun evaluate(it: Iterator<Char>): BigInteger {
+  var buffer = BigInteger.ZERO
+  var operation = '?'
+  while(it.hasNext()) {
+    when (val next = it.next()) {
+      ')' -> return buffer
+      in setOf('*', '+') -> operation = next
+      ' ' -> { }
+      else -> {
+        val term: BigInteger = if(next == '(') evaluate(it) else next.toString().toBigInteger()
+        when(operation) {
+          '?' -> buffer = term
+          '*' -> buffer *= term
+          '+' -> buffer += term
+        }
+      }
+    }
+  }
+  return buffer
+}
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`).useDelimiter("\n")
+
+  var r = BigInteger.ZERO
+  while(s.hasNext()) {
+    r += evaluate(s.next().iterator())
+  }
+  println(r)
+}
---
+++ problems/2020adventofcode18b/submissions/accepted/Stefan.kt
@@ -0,0 +1,40 @@
+import java.math.BigInteger
+import java.util.*
+
+fun evaluate(it: Stack<Char>, start: BigInteger = BigInteger.ZERO, readClosingBracket: Boolean = false): BigInteger {
+  var buffer = start
+  var operation = '?'
+  while(it.isNotEmpty()) {
+    when (val next = it.pop()) {
+      ')' -> {
+        if(!readClosingBracket) {
+          it.add(next)
+        }
+        return buffer
+      }
+      in setOf('*', '+') -> operation = next
+      ' ' -> { }
+      else -> {
+        val term: BigInteger = if(next == '(') evaluate(it, readClosingBracket = true) else next.toString().toBigInteger()
+        when(operation) {
+          '?' -> buffer = term
+          '*' -> buffer *= evaluate(it, start = term)
+          '+' -> buffer += term
+        }
+      }
+    }
+  }
+  return buffer
+}
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`).useDelimiter("\n")
+
+  var r = BigInteger.ZERO
+  while(s.hasNext()) {
+    val stack = Stack<Char>()
+    stack.addAll(s.next().toList().reversed())
+    r += evaluate(stack)
+  }
+  println(r)
+}
