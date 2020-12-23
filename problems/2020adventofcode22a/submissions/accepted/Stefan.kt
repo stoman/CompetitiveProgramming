091370a80889951827df7bfa8f2c989ca2a70013
Stefan Toman <git@stoman.de>
2020-12-23 02:31:41 +0100

    Add problems 2020adventofcode22a and 2020adventofcode22b

---
+++ problems/2020adventofcode22a/submissions/accepted/Stefan.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+  val decks = mutableMapOf<Int, Queue<Int>>()
+  var id = 0
+  var deck: Queue<Int> = LinkedList()
+  while (s.hasNext()) {
+    when (val next = s.next()) {
+      "Player" -> {
+        if (id > 0) decks[id] = deck
+        deck = LinkedList()
+        id = s.next().dropLast(1).toInt()
+      }
+      else -> deck.add(next.toInt())
+    }
+  }
+  decks[id] = deck
+
+  while (decks.values.count { it.isNotEmpty() } > 1) {
+    val nextCards = decks.mapValues { it.value.remove() }
+    val winner = nextCards.maxBy { it.value }!!.key
+    decks[winner]!!.addAll(nextCards.values.sortedDescending())
+  }
+
+  val winningDeck = decks.values.first() { it.isNotEmpty() }
+  var score = 0
+  while (winningDeck.isNotEmpty()) {
+    score += winningDeck.size * winningDeck.remove()
+  }
+  println(score)
+}
---
+++ problems/2020adventofcode22b/submissions/accepted/Stefan.kt
@@ -0,0 +1,54 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+  var deckA: ArrayDeque<Int> = ArrayDeque()
+  var deckB: ArrayDeque<Int> = ArrayDeque()
+  while (s.hasNext()) {
+    when (val next = s.next()) {
+      "Player" -> {
+        deckA = deckB
+        deckB = ArrayDeque()
+        s.next()
+      }
+      else -> deckB.addLast(next.toInt())
+    }
+  }
+
+  val winningDeck = if (play(deckA, deckB) == 0) deckA else deckB
+  var score = 0
+  while (winningDeck.isNotEmpty()) {
+    score += winningDeck.size * winningDeck.removeFirst()
+  }
+  println(score)
+}
+
+data class Key(val deckA: List<Int>, val deckB: List<Int>)
+
+fun play(deckA: ArrayDeque<Int>, deckB: ArrayDeque<Int>): Int {
+  val seen = mutableSetOf<Key>()
+
+  while (deckA.isNotEmpty() && deckB.isNotEmpty()) {
+    val nextA = deckA.removeFirst()
+    val nextB = deckB.removeFirst()
+    val key = Key(deckA.toCollection(mutableListOf()), deckB.toCollection(mutableListOf()))
+    if (key in seen) {
+      return 0
+    }
+    seen.add(key)
+
+    val winner = if (deckA.size >= nextA && deckB.size >= nextB) {
+      play(ArrayDeque(deckA.take(nextA)), ArrayDeque(deckB.take(nextB)))
+    } else {
+      if (nextA > nextB) 0 else 1
+    }
+    if (winner == 0) {
+      deckA.addLast(nextA)
+      deckA.addLast(nextB)
+    } else {
+      deckB.addLast(nextB)
+      deckB.addLast(nextA)
+    }
+  }
+  return if (deckA.isNotEmpty()) 0 else 1
+}
