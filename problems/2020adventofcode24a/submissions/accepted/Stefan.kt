75c07de1d3233fe0b692d2944326fa76d36f830c
Stefan Toman <git@stoman.de>
2020-12-25 00:33:27 +0100

    Add problems 2020adventofcode24a and 2020adventofcode24b

---
+++ problems/2020adventofcode24a/submissions/accepted/Stefan.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+  val tiles = mutableMapOf<Pair<Int, Int>, Boolean>()
+  while(s.hasNext()) {
+    var x = 0
+    var y= 0
+    val i = s.next().iterator()
+    while(i.hasNext()) {
+      when(i.nextChar()) {
+        'e' -> x++
+        'w' -> x--
+        'n' -> {
+          y++
+          if(i.nextChar() == 'w') {
+            x--
+          }
+        }
+        's' -> {
+          y--
+          if(i.nextChar() == 'e') {
+            x++
+          }
+        }
+      }
+    }
+    tiles[Pair(x, y)] = !(tiles[Pair(x, y)] ?: false)
+  }
+  println(tiles.values.count { it })
+}
---
+++ problems/2020adventofcode24b/submissions/accepted/Stefan.kt
@@ -0,0 +1,45 @@
+import java.util.*
+
+fun neighbors(tile: Pair<Int, Int>): List<Pair<Int, Int>> =
+  listOf(Pair(tile.first, tile.second - 1), Pair(tile.first + 1, tile.second - 1), Pair(tile.first + 1, tile.second),
+    Pair(tile.first - 1, tile.second), Pair(tile.first - 1, tile.second + 1), Pair(tile.first, tile.second + 1))
+
+fun main(args: Array<String>) {
+  val s = Scanner(System.`in`)
+  val tiles = mutableMapOf<Pair<Int, Int>, Boolean>()
+  while (s.hasNext()) {
+    var x = 0
+    var y = 0
+    val i = s.next().iterator()
+    while (i.hasNext()) {
+      when (i.nextChar()) {
+        'e' -> x++
+        'w' -> x--
+        'n' -> {
+          y++
+          if (i.nextChar() == 'w') {
+            x--
+          }
+        }
+        's' -> {
+          y--
+          if (i.nextChar() == 'e') {
+            x++
+          }
+        }
+      }
+    }
+    tiles[Pair(x, y)] = !(tiles[Pair(x, y)] ?: false)
+  }
+  var black = tiles.filter { it.value }.keys
+
+  repeat(100) {
+    val tilesToConsider = black.flatMap { neighbors(it) + listOf(it) }.toSet()
+    black = tilesToConsider.filter {
+      val cnt = neighbors(it).count { t -> t in black }
+      (it in black && (cnt == 1 || cnt == 2)) || (it !in black && cnt == 2)
+    }.toSet()
+  }
+
+  println(black.size)
+}
