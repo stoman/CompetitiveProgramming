3af2f647711f6bcb4016b79f308cfef5e72fbc02
Stefan Toman <git@stoman.de>
2021-12-04 09:50:23 +0100

    Add problems 2021adventofcode04a and 2021adventofcode04b

---
+++ problems/2021adventofcode04a/submissions/accepted/Stefan.kt
@@ -0,0 +1,49 @@
+import java.util.*
+
+private fun <T> List<List<T>>.transpose(): List<List<T>> {
+  if(isEmpty()) {
+    return listOf()
+  }
+  require(this.map { it.size }.toSet().size == 1) { "Matrix needs to have a square shape to be transposed" }
+  val ret = List(first().size) {mutableListOf<T>() }
+  for(list in this) {
+    for(i in list.indices) {
+      ret[i].add(list[i])
+    }
+  }
+  return ret
+}
+
+data class Board(val numbers: List<List<Int>>) {
+  fun isCompleted(draw: List<Int>): Boolean = numbers.any { draw.containsAll(it) } || numbers.transpose().any { draw.containsAll(it) }
+}
+
+@ExperimentalStdlibApi
+fun main() {
+  val s = Scanner(System.`in`)
+  val draw = buildList {
+    val ss = Scanner(s.next()).useDelimiter(",")
+    while (ss.hasNext()) {
+      add(ss.nextInt())
+    }
+  }
+  val boards = buildList {
+    while (s.hasNext()) {
+      val numbers = List(5) {mutableListOf<Int>() }
+      repeat(5) { row ->
+        repeat(5) { numbers[row].add(s.nextInt())}
+      }
+      add(Board(numbers))
+    }
+  }
+
+  for(drawIndex in draw.indices) {
+    val allDraws = draw.take(drawIndex + 1)
+    for(board in boards) {
+      if(board.isCompleted(allDraws)) {
+        println(allDraws.last() * board.numbers.flatten().filterNot { it in allDraws }.sum())
+        return
+      }
+    }
+  }
+}
